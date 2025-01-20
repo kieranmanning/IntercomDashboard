bffb42c0852627178d59b141ffff429cdd106b5f
Kieran Manning <mail@kmanning.ie>
2025-01-20 12:51:12 +0000

    will need to maintain a backend API after all for oauth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-//  import VerticalNavbar from './components/navbar'
 import './App.css'
 
 function App() {
